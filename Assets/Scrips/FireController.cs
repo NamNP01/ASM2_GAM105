0f990448456d6a599b460012e50c3ce639eed988
unknown <you@example.com>
2024-04-11 00:08:12 +0700

    lab7

--- Assets/Scrips/FireController.cs
+++ Assets/Scrips/FireController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FireController : MonoBehaviour
 {
+
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 2f);
+        player = GameObject.FindGameObjectWithTag("Player").transform; 
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        if (transform.position.x < player.position.x)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
     }
 }
